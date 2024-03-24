ce96222b5b2fd71b65609616dff79f64b8d8de62
Nuwan Chamara <nchamara91@gmail.com>
2024-03-24 10:43:37 +0530

    inital

---
+++ memoryhungryconsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Starting memory hungry...");
+        Timer timer = new Timer(IncreaseMemoryUsage, null, TimeSpan.Zero, TimeSpan.FromMinutes(5));
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+        
+    }
+
+    static void IncreaseMemoryUsage(object state)
+    {
+        // Allocate 100MB of memory
+        byte[] buffer = new byte[100 * 1024 * 1024];
+        Console.WriteLine("Increased memory usage by 100MB");
+    }
+}
